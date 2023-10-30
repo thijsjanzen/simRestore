93a6d159172be863b0ddee5b85bea9ff699f07b4
Thijs Janzen <thijsjanzen@gmail.com>
2023-10-30 12:08:32 +0100

    update documentation run_shiny_app

--- R/run_shiny_app.R
+++ R/run_shiny_app.R
@@ -1,11 +1,17 @@
 #' runs shiny app locally
+#' @description
+#' This function allows for local execution of the shiny app. Alternatively,
+#' an online version of this app
+#' \href{https://thijsjanzen.shinyapps.io/simRestoreApp/}{can be found here}.
+#'
+#' @return No return value
 #' @export
 run_shiny_app <- function() {
   appDir <- system.file("shiny-examples",
                         "simRestoreApp",
                         package = "simRestore")
   if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `mypackage`.",
+    stop("Could not find example directory. Try re-installing `simRestore`.",
          call. = FALSE)
   }
   shiny::runApp(appDir, display.mode = "normal")
